77010a1e1d4ab700e04ff03d52e6b8eea44eaf5c
Astrum-07 <astrumastro07@gmail.com>
2025-09-10 14:36:13 +0500

    hi bro

---
+++ misol.js
@@ -0,0 +1,59 @@
+// 1-misol
+// let num = +prompt("son kiriting")
+// if(num>0){
+//     console.log("Musbat son")
+// }else if(num<0){
+// console.log("Manfiy son")
+// }else{
+//     console.log("Xatolik")
+// }
+
+// 2-misol
+// let num = +prompt("Yoshingizni kiriting");
+// if (num >= 21) {
+//   console.log(" Siz ulg’aygansiz");
+// } else if (num < 21) {
+//   console.log(" siz hali yoshsiz");
+// } else {
+//   console.log("Siz tirikmisiz?");
+// }
+
+// 3-misol
+// let num = +prompt("kun kiriting");
+// if (num === 1) {
+//   console.log("Dushanba");
+// } else if (num === 2) {
+//   console.log("Seshanba");
+// } else if (num === 3) {
+//   console.log("Chorshanba");
+// } else if (num === 4) {
+//   console.log("Payshanba");
+// } else if (num === 5) {
+//   console.log("Juma");
+// } else if (num === 6) {
+//   console.log("Shanba");
+// } else if (num === 7) {
+//   console.log("Yakshanba");
+// } else {
+//   console.log("Xatolik! 1 dan 7 gacha son kiriting.");
+// }
+
+// 4-misol
+// let num = +prompt("Tempraturangizni kiriting");
+// if (num >= 30) {
+//   console.log(" Sizni tempraturangiz issiq");
+// } else if (num < 30) {
+//   console.log("  Sizni tempraturangiz sovuq");
+// } else {
+//   console.log("Siz tirikmisiz?");
+// }
+
+// 5-misol
+// let son = prompt("son kiriting");
+// if (son % 2 === 0) {
+//   console.log("juft son");
+// } else if (son % 2 === 1) {
+//   console.log("toq son");
+// } else {
+//   console.log("xatolik");
+// }
